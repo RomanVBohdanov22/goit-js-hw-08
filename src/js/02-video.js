b08133a317bea219fb4559b890e75baa53e5b05d
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-17 14:15:19 +0200

    some new modules

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
 //npm install @vimeo/player
+
+//import SimpleLightbox from 'simplelightbox';
+import _ from 'lodash';
+
+//const result = _.add(2, 3);
+//console.log(result); // 5
+//alert(result);
+
+//var throttle = require('_.throttle');
