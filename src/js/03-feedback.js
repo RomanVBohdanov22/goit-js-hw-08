19eb34d2a697d6ef97ea23cff57e012f271ec859
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-18 16:53:22 +0200

    need some treak)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+/*
+<form class="feedback-form" autocomplete="off">
+  <label>
+    Email
+    <input type="email" name="email" autofocus />
+  </label>
+  <label>
+    Message
+    <textarea name="message" rows="8"></textarea>
+  </label>
+  <button type="submit">Submit</button>
+</form>
+*/
 
+import _ from 'lodash';
+const STORAGE_KEY = "feedback-form-state";
+const parsedDataLocal = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener("input", _.throttle(formReader, 500));
+
+function formReader(event) { ; }
