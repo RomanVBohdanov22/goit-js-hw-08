83ae18da68857d442e06be2396005f96b211e606
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-18 22:44:27 +0200

    task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,10 +14,71 @@
 
 import _ from 'lodash';
 const STORAGE_KEY = "feedback-form-state";
-const parsedDataLocal = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
-const feedbackForm = document.querySelector('.feedback-form');
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error("Set state error: ", error.message);
+  }
+};
 
-feedbackForm.addEventListener("input", _.throttle(formReader, 500));
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
 
-function formReader(event) { ; }
+let toData = {};
+const parsedDataLocal = load(STORAGE_KEY);
+
+const refsToForm = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    email: document.querySelector('.feedback-form input'),
+};
+
+refsToForm.form.addEventListener('submit', onFormSubmit);
+refsToForm.form.addEventListener('input', _.throttle(onInput, 500));
+refsToForm.form.addEventListener('input', e =>
+{ toData[e.target.name] = e.target.value; });
+
+onLoadForm();
+
+function onFormSubmit(e) { 
+    e.preventDefault();
+    console.log(parsedDataLocal);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+function onInput(e)
+{
+    return save(STORAGE_KEY, toData);
+}
+
+function onLoadForm() { 
+    if (load(STORAGE_KEY))
+    {
+             
+        const outputForm = load(STORAGE_KEY);
+        const formKeys = Object.keys(outputForm);
+
+        formKeys.map(element => {
+            document.querySelector(`[name='${element}']`).value = outputForm[element];
+        });
+        //const savedForm = load(STORAGE_KEY);
+        //const formKeys = Object.keys(savedForm);
+            /*
+        formKeys.map( element => {
+            document.querySelector(
+                `name='${element}'`
+            ).value = savedForm[element];
+        });*/
+
+        }
+}
+
