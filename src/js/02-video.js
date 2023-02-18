e4c33dd64ea913679f50a6a978aea954c86376d1
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-02-18 12:36:23 +0200

    lets go my dear

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 //npm install @vimeo/player
 import Player from '@vimeo/player';
-import { throttle } from 'lodash';
+import _ from 'lodash';
 
 //import _ from 'lodash';
 //const result = _.add(2, 3);
@@ -19,7 +19,32 @@ import { throttle } from 'lodash';
 ></iframe>
  */
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () { console.log("It's works!"); });
+player.on('play', function () {
+  console.log("It's plays!");
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const currentTime = function (data) {
+    /*
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JOSN.stringify(data.seconds)
+  );
+    let time = Number(localStorage.getItem('videoplayer-current-time'));
+  console.log(time);*/
+    let time = data.seconds;
+    console.log(time);
+    //localStorage.setItem('videoplayer-current-time', JOSN.stringify(data.seconds));
+};
+
+player.on("timeupdate", _.throttle(currentTime, 1000));//_.throttle(() => { currentTime }, 1000)); 
+
+//throttle(() => { console.log("Scroll handler call every 300ms");  }, 300)
+    
+  
